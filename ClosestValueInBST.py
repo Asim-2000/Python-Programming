503a638a19fdb93809165f544fe869a4c33a52e3
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-23 00:59:53 +0500

    Closest value in BST added

--- ClosestValueInBST.py
+++ ClosestValueInBST.py
@@ -1,3 +1,7 @@
+#Average Case: Time = O(Log(n)) || Space = O(Log(n))
+# Worst Case: Time = O(N) || Space = O(N)
+"""Recursive implementation"""
+
 def FindClosestValueinBST(tree,target):
     return FindingHelper(tree,target,float("inf"))
 
@@ -10,6 +14,26 @@ def FindingHelper(tree,target,closest):
         return (FindingHelper(tree.left,target,closest))
     elif (target > tree.value):
         return (FindingHelper(tree.right,target,closest))
+    else:
+        return closest 
 
-    return closest 
-    
+#Average Case: Time = O(Log(n)) || Space = O(1)
+# Worst Case: Time = O(N) || Space = O(1)
+"""Iterative Implementation"""
+
+def FCVBSTIterative(tree, target):
+    return HelperFunction(tree,target,float("inf"))
+
+def HelperFunction(tree,target,closest):
+    currentNode=tree
+    while currentNode is not None:
+        if abs(target-closest) > abs (target-currentNode.value):
+            closest=currentNode.value
+        if (target < currentNode.value):
+            currentNode=currentNode.left
+        elif (target > currentNode.value):
+            currentNode=currentNode.right
+        else:
+            break
+
+        return closest
