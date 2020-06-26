1f883a61d05119673671f08752bafdc8176ec047
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-27 01:24:02 +0500

    another implementation added

--- Caesar Cipher Encryptor.py
+++ Caesar Cipher Encryptor.py
@@ -1,3 +1,5 @@
+"""Implementation no.1"""
+
 # Time=O(N) || Space = O (N)
 def CaesarCipherEncryptor(string,key):
     newLetters = []
@@ -10,3 +12,21 @@ def CaesarCipherEncryptor(string,key):
 def getNewLetter(letter,key):
     newLetterCode=ord(letter) + key
     return chr(newLetterCode) if newLetterCode<=122 else chr(96+newLetterCode%122)
+
+
+
+"""Implementation no.2"""
+
+# Time=O(N) || Space = O (N)
+def CaesarCipherEncryptor(string,key):
+    newLetters = []
+    newKey= key % 26
+    alphabets=list("abcdefghijklmnopqrstuvwxyz")
+    for letter in string:
+        newLetters.append(getNewLetter(letter,newKey,alphabets))
+    return "".join(newLetters)
+
+
+def getNewLetter(letter,key,alphabets):
+    newLetterCode=alphabets.index(letter)+ key
+    return alphabets[newLetterCode] if newLetterCode<=25 else alphabets(-1+newLetterCode%25)
