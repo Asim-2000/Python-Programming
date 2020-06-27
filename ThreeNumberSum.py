72f4c0b7f9ba772c94824b294adb9d0b4e94924c
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-27 14:52:38 +0500

    three Number Sum Added

--- ThreeNumberSum.py
+++ ThreeNumberSum.py
@@ -1,7 +1,21 @@
+#Time=O(N^2) || Space= O(N)
 def ThreeNumSum(array,targetSum):
     array.sort()
     triplets=[]
     #Since we want triplet, we want the fixed number to go as far as the 3rd last value in array
     #so that we can have left and right pointer too.
     for i in range(len(array)-2):
+        left=i+1
+        right=len(array)-1
+        while left < right:
+            CurrentSum= array[i]+array[left]+array[right]
+            if CurrentSum==targetSum:
+                triplets.append([array[i],array[left],array[right]])
+                left+=1
+                right-=1
+            elif CurrentSum < targetSum:
+                left+=1
+            elif CurrentSum > targetSum:
+                right-=1
         
+    return triplets
