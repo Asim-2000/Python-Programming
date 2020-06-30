b03c7031b775698451926a154a7dabaff1f1844b
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-06-30 15:04:34 +0500

    smallest difference added

---
+++ Medium/SmallestDifference.py
@@ -0,0 +1,25 @@
+def SmallestDifference(array1,array2):
+    array1.sort()
+    array2.sort()
+    idx1=0
+    idx2=0
+    smallest=float('inf')
+    current=float('inf')
+    smallestpair=[]
+    
+    while (idx1 < len(array1) and idx2 < len(array2)):
+        firstnum=array1[idx1]
+        secondnum=array2[idx2]
+        if firstnum < secondnum :
+            current= secondnum - firstnum
+            idx1+=1
+        elif firstnum > secondnum :
+            current=firstnum-secondnum
+            idx2+=1
+        # else condition is firstnum==secondnum which can be the smallest difference     
+        else:
+             return [firstnum,secondnum]
+        if current<smallest:
+            smallest=current
+            smallestpair=[firstnum,secondnum]
+    return smallestpair
