1c61bf3e06427bb4482ae7ae1a2abf5d2f79ad88
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-07-11 00:07:12 +0500

    Done with first Algo

--- Dynamic Programming/Max Subset (No Adjacent).py
+++ Dynamic Programming/Max Subset (No Adjacent).py
@@ -4,4 +4,7 @@ def maxSubsetSumNoAdjacent(array):
     elif (len(array))==1:
         return array[0]
     maxSums=array[:]
-    maxSums[1]=max
+    maxSums[1]=max(array[0],array[1])
+    for i in range(2,len(array)):
+        maxSums[i]=max(maxSums[i-1],maxSums[i-2]+array[i])
+    return maxSums[-1]
