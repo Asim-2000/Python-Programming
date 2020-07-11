02d948172251620c8def2ca4bc6d2f70988dde79
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-07-11 18:54:49 +0500

    Almost Done

--- Dynamic Programming/Number of Ways to make change.py
+++ Dynamic Programming/Number of Ways to make change.py
@@ -1,2 +1,8 @@
-def NumberOfWaysToMakeChange(n,denoms):
-    
+def NumberOfWaysToMakeChange(n,coins):
+    ways=[0 for ammount in range(n+1)]
+    ways[0]=1
+    for coin in coins:
+        for ammount in range(1,n+1):
+            if coin <= ammount:
+                
+                
