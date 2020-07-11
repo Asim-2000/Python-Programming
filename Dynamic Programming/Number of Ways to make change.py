7d9f63178ad21dc9474bddb15f305954a3cd5639
Asim <mkhaskheli.bese18seecs@seecs.edu.pk>
2020-07-11 19:01:51 +0500

    Space and time added

--- Dynamic Programming/Number of Ways to make change.py
+++ Dynamic Programming/Number of Ways to make change.py
@@ -1,11 +1,14 @@
+# Time=(Nd) | Space = o(N)
 def NumberOfWaysToMakeChange(n,coins):
     ways=[0 for ammount in range(n+1)]
     ways[0]=1
     for coin in coins:
+
         for ammount in range(1,n+1):
+            
             if coin <= ammount:
                 ways[ammount]+=ways[ammount-coin]
     
     return ways[n]
-    
+
                 
